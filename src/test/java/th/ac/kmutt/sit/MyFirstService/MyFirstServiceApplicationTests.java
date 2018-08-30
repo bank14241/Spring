135e1d3f58c4235eb8f476a8951ac72de63b6648
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-08-30 08:55:22 +0700

    [DELETE] unused import

--- src/test/java/th/ac/kmutt/sit/MyFirstService/MyFirstServiceApplicationTests.java
+++ src/test/java/th/ac/kmutt/sit/MyFirstService/MyFirstServiceApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 import th.ac.kmutt.sit.MyFirstService.controller.User;
-import th.ac.kmutt.sit.MyFirstService.controller.UserController;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
